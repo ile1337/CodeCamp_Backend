6668919c9350ee063ace195b239a60d1d7880ea3
agonosmani <agon_osmani@live.com>
2023-05-14 15:12:24 +0200

    Initial commit

---
+++ src/test/java/mk/ukim/finki/codecamp/CodecampApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.codecamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodecampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
