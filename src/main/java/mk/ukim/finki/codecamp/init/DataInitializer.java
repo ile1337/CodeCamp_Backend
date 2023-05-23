1f12a1da39aa548c9e0cde407c50097044037553
agonosmani <agon_osmani@live.com>
2023-05-23 15:09:49 +0200

    Configure Database

--- src/main/java/mk/ukim/finki/codecamp/init/DataInitializer.java
+++ src/main/java/mk/ukim/finki/codecamp/init/DataInitializer.java
@@ -1,13 +1,24 @@
 package mk.ukim.finki.codecamp.init;
 
 import jakarta.annotation.PostConstruct;
+import mk.ukim.finki.codecamp.service.CourseService;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
 
 @Component
 public class DataInitializer {
 
+    private final CourseService courseService;
+
+    public DataInitializer(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
     @PostConstruct
     public void init() {
-
+        courseService.createEmpty("Summer", "ICT Projects");
+        courseService.createEmpty("Summer", "Kognitivni Nauki");
+        courseService.createEmpty("Winter", "Databases");
     }
 }
